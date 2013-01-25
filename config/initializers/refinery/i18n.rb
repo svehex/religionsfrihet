f087c23c8600fe197d3a59f4a0cd839754459b37
Harald Eilertsen <haraldei@anduin.net>
2013-01-25 21:53:05 +0100

    Initial commit.

---
+++ config/initializers/refinery/i18n.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+Refinery::I18n.configure do |config|
+  config.enabled = true
+  config.default_locale = :nb
+  config.current_locale = :nb
+  config.default_frontend_locale = :nb
+  config.frontend_locales = [:nb, :en]
+  config.locales = {:en=>"English", :fr=>"Français", :nl=>"Nederlands", :"pt-BR"=>"Português", :da=>"Dansk", :nb=>"Norsk Bokmål", :sl=>"Slovenian", :es=>"Español", :it=>"Italiano", :de=>"Deutsch", :lv=>"Latviski", :ru=>"Русский", :sv=>"Svenska", :pl=>"Polski", :"zh-CN"=>"Simplified Chinese", :"zh-TW"=>"Traditional Chinese", :el=>"Ελληνικά", :rs=>"Srpski", :cs=>"Česky", :sk=>"Slovenský", :ja=>"日本語", :bg=>"Български"}
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReligionsfrihetNo::Application.config.secret_token = '326727027aa26095e98aeccc39abfdabb117fb93d843ea3f89330f6120bcbd3a56566124c3667d554793783b45a0d1aed68b4923944032c14ee40752870667d5'
